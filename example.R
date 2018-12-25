7df0473866c8ecdc34a92a4133a08ca7375d4d11
BigFaceCat <yhebfc@gmail.com>
2018-12-25 04:52:11 -0800

    add example file

---
+++ example.R
@@ -0,0 +1,31 @@
+# one directory with every file the app needs
+# app.R (you must use the exact name); or two file server.R and ui.R
+
+# Shinyapps.io: free R server by RStudio
+# my account is https://hybfc.shinyapps.io
+
+# devtools::install_github --install from github
+# rsconnect package enables you to deploy and manage your Shiny applications directly from your R console
+
+library(shiny)
+
+ui <- fluidPage(
+  # *Input() functions
+  sliderInput(inputId = "num", label = "choose sample size", value = 50, min= 10, max=1000),
+  # *Output() functions
+  plotOutput(outputId = "hist")
+)
+
+server <- function(input, output) {
+  # Three rules to write server function
+  # 1, save objects display to output$
+  # 2, use render function to create the needed type of output
+  #    (use {} to include multiple commands)
+  # 3, use input values with input$
+  output$hist <- renderPlot({
+    hist(rnorm(input$num), main="")
+    title("histogram")
+    })
+}
+
+shinyApp(ui = ui, server = server)
